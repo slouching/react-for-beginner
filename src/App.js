1f04f74dcc9849c414441a900bf744f609caa73b
Slouching <dulnik@yandex.ru>
2022-11-25 14:50:21 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
+import './index.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0)
+
+    const decrease = () => {
+        setCount(count - 1)
+    }
+
+    const increase = () => {
+        setCount(count + 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <h1 className="title">Counter:</h1>
+                <h2 className="output">{count}</h2>
+                <button onClick={decrease} className="decrease">- Decrease</button>
+                <button onClick={reset} className="reset">Reset</button>
+                <button onClick={increase} className="increase">Increase +</button>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
